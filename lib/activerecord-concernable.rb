9378d9144633523d012d61939034245a39e50574
Blake Taylor <blakefrost@gmail.com>
2013-01-30 13:25:19 -0800

    Setup project

---
+++ lib/activerecord-concernable.rb
@@ -0,0 +1,7 @@
+require "activerecord-concernable/version"
+
+module Activerecord
+  module Concernable
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord-concernable/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module Concernable
+    VERSION = "0.0.1"
+  end
+end
